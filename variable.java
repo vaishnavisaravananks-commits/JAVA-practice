529dc4be4b1e6075e3db154dd68a8f0c88fb0b24
vaishnavisaravananks-commits <vaishnavisaravananks@gmail.com>
2026-05-23 22:39:19 +0530

    practiced variables through student bio data

---
+++ variable.java
@@ -0,0 +1,20 @@
+public class variable {
+    public static void main(String args[]){
+        System.out.println("Student Bio Data");
+        String studentName="Vaish";
+        int studentAge=20;
+        float studentFee=1.50f;
+        String Dept="CSE";
+         System.out.println("Student Name: "+studentName);
+          System.out.println(" Age: "+studentAge);
+           System.out.println("Fees: "+studentFee);
+            System.out.println("Department: "+Dept);
+           
+
+           
+           
+
+
+    }
+    
+}
