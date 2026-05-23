2096a6c76d5927ed84598f4d9a7df48d91fe10f4
vaishnavisaravananks-commits <vaishnavisaravananks@gmail.com>
2026-05-23 22:14:35 +0530

    practiced datatypes

---
+++ Datatype.java
@@ -0,0 +1,20 @@
+public class datatype {
+    public static void main(String args[]){
+    System.out.println("Let us list some datatypes");
+    //int
+    int a=5;
+    System.out.println("int datatype " +a);
+    //float
+    float b=2.2f;
+     System.out.println("float datatype " +b);
+     //boolean
+     boolean isjavafun=true;
+     boolean isfishsmellsgood=false;
+     System.out.println("Is Java Fun? " +isjavafun);
+     System.out.println("Is Fish smells good? " +isfishsmellsgood);
+    
+    }
+
+
+    
+}
