e57804344d8ac6e9982112a8020a529e3e732302
vaishnavisaravananks-commits <vaishnavisaravananks@gmail.com>
2026-05-23 19:48:48 +0530

    differentiated how java print output in new and same lines

---
+++ output.java
@@ -0,0 +1,14 @@
+public class output {
+     public static void main(String[] args) {
+    //to ouptut code in new lines
+    System.out.println("HEY");
+    System.out.println("How are you?");
+    //to ouput code in same line
+    System.out.print("HEY");
+    System.out.print("How are you?");
+
+   
+        
+    }
+    
+}
