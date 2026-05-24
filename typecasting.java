ef8bb94dd20e41d4ae0bcd996e2e8bb7b94455ef
vaishnavisaravananks-commits <vaishnavisaravananks@gmail.com>
2026-05-24 22:39:39 +0530

    Update typecasting.java

--- typecasting.java
+++ typecasting.java
@@ -1,10 +1,10 @@
 public class typecasting {
     public static void main(String[] args) {
     //narrowing casting(convert large type into small type)
-    int myNum=5;
-    double myDouble=(double)myNum;
-    System.out.println(myNum);
+    double myDouble=0.724591038275642d;
+    int myInt=(int)myDouble;
     System.out.println(myDouble);
+    System.out.println(myInt);
     
 }
 }
