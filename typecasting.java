edd226133e1283491854dcd6ee2d6528b8534cfe
vaishnavisaravananks-commits <vaishnavisaravananks@gmail.com>
2026-05-24 20:59:36 +0530

    narrow typecating

---
+++ typecasting.java
@@ -0,0 +1,10 @@
+public class typecasting {
+    public static void main(String[] args) {
+    //narrowing casting(convert large type into small type)
+    int myNum=5;
+    double myDouble=(double)myNum;
+    System.out.println(myNum);
+    System.out.println(myDouble);
+    
+}
+}
