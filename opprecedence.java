5e77ec8d7a4ad47b0faea65f7f9e5465f6b6f812
vaishnavisaravananks-commits <vaishnavisaravananks@gmail.com>
2026-05-25 12:42:53 +0530

    Performed opearor precedence for expressions

---
+++ opprecedence.java
@@ -0,0 +1,9 @@
+public class opprecedence {
+    public static void main(String[] args) {
+        int a,b,c;
+        a=10;b=5;c=2;
+        int result=10+5*2;
+        System.out.println("The result is: "+result);
+    }
+    
+}
