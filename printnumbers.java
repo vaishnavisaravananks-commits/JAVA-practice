1e2a94ea1250be6af71d44e8d033434471bf999b
vaishnavisaravananks-commits <vaishnavisaravananks@gmail.com>
2026-05-26 18:17:21 +0530

    printing numbers from 1 to 10 using for loop

---
+++ printnumbers.java
@@ -0,0 +1,12 @@
+public class printnumbers {
+    public static void main(String args[]){
+        System.out.println("Printing Numbers from 1 to 10");
+    
+        for(int i =1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+
+    
+    
+}
