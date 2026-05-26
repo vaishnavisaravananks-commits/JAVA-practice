e356fcb280ed89099f1751f0b3c560672d99b32f
vaishnavisaravananks-commits <vaishnavisaravananks@gmail.com>
2026-05-26 18:57:55 +0530

    Codechallenge1

---
+++ codechallenge1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class codechallenge1 {
+    public static void main(String args[]){
+    //getting variable from user and print it
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter your name: ");
+    String name=sc.next();
+    System.out.println("Enter your age: ");
+    int age=sc.nextInt();
+    System.out.println("HI "+name);
+    System.out.println("your age is: "+age);
+
+    }
+    
+    
+}
